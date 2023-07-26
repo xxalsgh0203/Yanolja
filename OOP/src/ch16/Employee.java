3f85da44431f18ae98cbe445a1577783141d7c0e
Minho Kim <music4537@naver.com>
2023-07-26 14:37:38 +0900

    static 변수

---
+++ OOP/src/ch16/Employee.java
@@ -0,0 +1,43 @@
+package ch16;
+
+public class Employee {
+    private int employeeId;
+    private String employeeName;
+    private String department;
+
+    public static int serialNum = 1000;
+
+    public Employee() {
+        this.employeeId = serialNum;
+        serialNum++;
+    }
+
+    public String toString(){
+        return (this.employeeName + "님의 ID는 " + this.employeeId);
+    }
+
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
---
+++ OOP/src/ch16/EmployeeTest.java
@@ -0,0 +1,14 @@
+package ch16;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee employeeLee = new Employee();
+        employeeLee.setEmployeeName("이순신");
+        Employee employeeKim = new Employee();
+        employeeKim.setEmployeeName("김유신");
+
+        // System.out.println(employeeLee.serialNum);
+        System.out.println(employeeKim); // same as employeeKim.toString();
+        System.out.println(employeeLee);
+    }
+}
