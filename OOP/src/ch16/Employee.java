f7fc44f5a81a32c4db35e9a42edd30b0d8ce0e30
Minho Kim <music4537@naver.com>
2023-07-26 14:57:38 +0900

    static 변수

--- OOP/src/ch16/Employee.java
+++ OOP/src/ch16/Employee.java
@@ -5,11 +5,18 @@ public class Employee {
     private String employeeName;
     private String department;
 
-    public static int serialNum = 1000;
+    private static int serialNum = 1000;
+    // static 변수는 프로그램이 메모리에 있는 동안 계속 그 영역을 차지하므로 너무 큰 메모리를 할당하는 것은 좋지 않다.
+    // 프로그램이 메모리에 올라가면 프로세스가 되고 프로세스가 종료되야 사라짐.
+    // 인스턴스들 끼리 정말 고유되어야 하는 데이터가 있을때 static 을 사용하면 됨.
 
     public Employee() {
         this.employeeId = serialNum;
         serialNum++;
+    }
+
+    public static int getSerialNum() {
+        return serialNum;
     }
 
     public String toString(){
--- OOP/src/ch16/EmployeeTest.java
+++ OOP/src/ch16/EmployeeTest.java
@@ -7,6 +7,8 @@ public class EmployeeTest {
         Employee employeeKim = new Employee();
         employeeKim.setEmployeeName("김유신");
 
+        System.out.println(Employee.getSerialNum());
+
         // System.out.println(employeeLee.serialNum);
         System.out.println(employeeKim); // same as employeeKim.toString();
         System.out.println(employeeLee);
