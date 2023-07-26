7a56b4e239a36f7ba3700e5e5a1d4841dffbf244
Minho Kim <music4537@naver.com>
2023-07-26 14:58:44 +0900

    static 변수 & 메소드

--- OOP/src/ch16/EmployeeTest.java
+++ OOP/src/ch16/EmployeeTest.java
@@ -2,12 +2,13 @@ package ch16;
 
 public class EmployeeTest {
     public static void main(String[] args) {
+
+        System.out.println(Employee.getSerialNum()); // serialNum 은 static 변수이기 때문에 인스턴스 생성하지 않아도 됨
+
         Employee employeeLee = new Employee();
         employeeLee.setEmployeeName("이순신");
         Employee employeeKim = new Employee();
         employeeKim.setEmployeeName("김유신");
-
-        System.out.println(Employee.getSerialNum());
 
         // System.out.println(employeeLee.serialNum);
         System.out.println(employeeKim); // same as employeeKim.toString();
