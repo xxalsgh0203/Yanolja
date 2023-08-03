f5ee931b68cb2f6b3c3ef7adb548720525a29f22
Minho Kim <music4537@naver.com>
2023-08-03 17:16:47 +0900

    직접접근, 간접접근

--- JavaBasic/src/kr/fc/java/FC16.java
+++ JavaBasic/src/kr/fc/java/FC16.java
@@ -1,2 +1,15 @@
-package kr.fc.java;public class FC16 {
+package kr.fc.java;
+
+import kr.fc.poly1.Animal;
+import kr.fc.poly1.Dog;
+
+public class FC16 {
+    // Dog 객체를 생성하는 방법 3가지
+
+    // 1.
+    Dog d2 = new Dog(); // 직접 접근
+    // 2.
+    Animal d1 = new Dog(); // 간접 접근(Upcasting)
+    // 3.
+    Object d3 = new Dog(); // 자바에서 제공하는 root 클래스
 }
