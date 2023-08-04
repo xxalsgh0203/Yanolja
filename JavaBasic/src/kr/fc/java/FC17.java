88f546ccb47f1b148b23a23ad8500a18bbbadbfe
Minho Kim <music4537@naver.com>
2023-08-04 11:25:43 +0900

    다형성 배열

--- JavaBasic/src/kr/fc/java/FC17.java
+++ JavaBasic/src/kr/fc/java/FC17.java
@@ -1,2 +1,24 @@
-package kr.fc.java;public class FC17 {
+package kr.fc.java;
+
+import kr.fc.poly1.Animal;
+import kr.fc.poly1.Cat;
+import kr.fc.poly1.Dog;
+
+public class FC17 {
+    public static void main(String[] args) {
+        // 1. 다형성 배열
+        // Dog, Cat 을 저장할 배열을 생성하세요
+        Animal[] animals = new Animal[2];
+        animals[0] = new Dog(); // eat()
+        animals[1] = new Cat(); // eat(), night()
+
+        for(Animal animal : animals){
+            if(animal instanceof Cat){
+                ((Cat) animal).night();
+            }
+            animal.eat();
+        }
+
+
+    }
 }
