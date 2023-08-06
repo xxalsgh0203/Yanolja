8cf43039d437129a26380a28730e8e44aae736bb
Minho Kim <music4537@naver.com>
2023-08-06 23:13:07 +0900

    다형성보장이 되는 추상클래스

--- JavaBasic/src/kr/fc/java/FC20.java
+++ JavaBasic/src/kr/fc/java/FC20.java
@@ -1,2 +1,15 @@
-package kr.fc.java;public class FC20 {
+package kr.fc.java;
+
+import kr.fc.poly2.*;
+
+public class FC20 {
+    public static void main(String[] args) {
+        //        Animal ani = new Animal();   -> 오류. 추상클래스는 객체로 사용할 수 없음. 부모역할로 사용해야 함
+        Animal ani = new Dog();
+        ani.eat(); // 다형성이 보장
+        ani = new Cat();
+        ani.eat(); // 다형성이 보장
+
+        ((Cat)ani).night();
+    }
 }
