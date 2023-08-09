fe0abb9e1fbb3ebb1d35f223e7699a32ef6f5623
Minho Kim <music4537@naver.com>
2023-08-09 11:34:30 +0900

    인터페이스 기반의 jdbc

--- JavaBasic/src/kr/fc/java/FC22.java
+++ JavaBasic/src/kr/fc/java/FC22.java
@@ -1,2 +1,17 @@
-package kr.fc.java;public class FC22 {
+package kr.fc.java;
+
+import kr.fc.jdbc.JdbcConnection;
+import kr.fc.jdbc.MySQLConnection;
+import kr.fc.jdbc.OracleConnection;
+
+public class FC22 {
+    public static void main(String[] args) {
+        // Oracle DBMS 접속
+        JdbcConnection oracleConn = new OracleConnection();
+        oracleConn.getConnection("URL", "admin", "1234");
+
+        // MySQL DBMS 접속
+        JdbcConnection mysqlConn = new MySQLConnection();
+        mysqlConn.getConnection("URL", "admin", "1234");
+    }
 }
