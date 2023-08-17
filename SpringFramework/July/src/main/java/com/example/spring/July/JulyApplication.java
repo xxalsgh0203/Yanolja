de091afd12afb20227633f915f1d67519592e251
Minho Kim <music4537@naver.com>
2023-08-17 14:37:16 +0900

    Spring project 생성

---
+++ SpringFramework/July/src/main/java/com/example/spring/July/JulyApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.July;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulyApplication.class, args);
+	}
+
+}
---
+++ SpringFramework/July/src/test/java/com/example/spring/July/JulyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.July;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
