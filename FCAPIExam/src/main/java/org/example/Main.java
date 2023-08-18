9d06324d1d124a2e637c1050a4f1461e351596be
Minho Kim <music4537@naver.com>
2023-08-18 15:38:58 +0900

    API project init

---
+++ FCAPIExam/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+// maven(pom.xml), gradle(build.gradle)
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
