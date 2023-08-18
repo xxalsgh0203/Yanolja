3515d5402b2cb713989aad8294ab6da3d3bc98be
Minho Kim <music4537@naver.com>
2023-08-18 21:57:18 +0900

    이미지 읽고 복제하기

--- JavaBasic/src/kr/fc/java/FC38.java
+++ JavaBasic/src/kr/fc/java/FC38.java
@@ -1,2 +1,29 @@
-package kr.fc.java;public class FC38 {
+package kr.fc.java;
+
+import java.io.*;
+
+public class FC38 {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("pic.PNG");
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            FileOutputStream fos = new FileOutputStream("newpic.PNG");
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            // 0 ~ 255
+            int data;
+            int cnt = 0;
+            while((data = fis.read()) != -1){
+                bos.write(data);
+//                System.out.println(data);
+//                cnt++;
+                bos.flush();
+            }
+            bos.close();
+            fis.close();
+            System.out.println("이미지 복사 완료");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
