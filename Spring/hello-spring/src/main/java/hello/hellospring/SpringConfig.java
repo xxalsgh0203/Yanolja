0e42b2ad4978c2fb96b18cd9d7214e5b3780242d
Minho Kim <music4537@naver.com>
2023-08-24 11:40:19 +0900

    회원 웹 기능 - 등록

--- Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -6,15 +6,15 @@ import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class SpringConfig {
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
-}
+//@Configuration
+//public class SpringConfig {
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
+//}
