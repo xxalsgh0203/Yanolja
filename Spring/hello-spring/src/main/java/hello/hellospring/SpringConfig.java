d3e8e87ac5eee0d592671536f5c2dfc3a31b5956
Minho Kim <music4537@naver.com>
2023-08-25 00:26:23 +0900

    스프링 jdbc template

--- Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -12,7 +13,8 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-
+    // DataSource는 데이터베이스 커넥션을 획득할 때 사용하는 객체다.
+    // 스프링 부트는 데이터베이스 커넥션 정보를 바탕으로 DataSource를 생성하고 스프링 빈으로 만들어둔다. 그래서 DI를 받을 수 있다
     private DataSource dataSource;
 
     @Autowired
@@ -27,7 +29,8 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
