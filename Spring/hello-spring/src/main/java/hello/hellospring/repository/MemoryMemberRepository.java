da0130d160ae45db6d361ac0d97e854561afab87
Minho Kim <music4537@naver.com>
2023-08-24 22:56:37 +0900

    순수 JDBC

--- Spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -4,7 +4,7 @@ import hello.hellospring.domain.Member;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
-@Repository
+//@Repository
 public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L; // 0,1,2,, 키값 생성해주는 애
