1a0b87a06d7f0561345aac039e48dc973d6650de
Minho Kim <music4537@naver.com>
2023-08-28 15:37:40 +0900

    JDBC SELECT SQL

--- FCAPIExam/src/main/java/org/example/JDBC04.java
+++ FCAPIExam/src/main/java/org/example/JDBC04.java
@@ -1,5 +1,10 @@
 package org.example;
+import org.example.model.Book;
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JDBC04 {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/fcampus";
@@ -7,16 +12,29 @@ public class JDBC04 {
         String password = "1q2w3e4r!";
         // 책 전체 리스트를 출력하세요.
         String SQL = "select * from tblbook";
+        List<Book> blist = new ArrayList<>();
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(url, username, password);
             Statement st = conn.createStatement();
-            int cnt = st.executeUpdate(SQL);
-            if(cnt > 0){
-                System.out.println("삭제 완료:" + cnt);
-            }else{
-                System.out.println("수정 실패");
+            // int cnt = st.executeUpdate(SQL) : insert, update, delete
+            ResultSet rs = st.executeQuery(SQL);
+            while(rs.next()){
+                int num = rs.getInt("num");
+                String title = rs.getString("title");
+                String company = rs.getString("company");
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+
+                // 묶고(VO) -> 담고(List)
+                Book book = new Book(num, title, company, name, price);
+                blist.add(book);
             }
+
+            for(Book book : blist){
+                System.out.println(book.toString());
+            }
+
         } catch(Exception e){
             e.printStackTrace();
         }
