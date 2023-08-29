6118308246c2faac5ce57c87e93b8d1d03a46ca6
Minho Kim <music4537@naver.com>
2023-08-29 11:49:23 +0900

    DAO 생성 &  분리

--- FCAPIExam/src/main/java/org/example/JDBC07.java
+++ FCAPIExam/src/main/java/org/example/JDBC07.java
@@ -1,2 +1,21 @@
-package org.example;public class JDBC07 {
+package org.example;
+
+import org.example.model.Book;
+import org.example.model.BookDAO;
+
+import java.util.List;
+
+public class JDBC07 {
+    public static void main(String[] args) {
+        BookDAO dao = new BookDAO();
+        List<Book> blist = dao.bookList();
+
+        if(blist.size() != 0){
+            for(Book book : blist){
+                System.out.println(book);
+            }
+        }else{
+            System.out.println("데이터가 없습니다.");
+        }
+    }
 }
