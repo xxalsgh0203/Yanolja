3a36107f490e0b59d882c05cfa3b220e902f83f5
Minho Kim <music4537@naver.com>
2023-08-29 11:49:08 +0900

    DAO 생성 &  분리

--- FCAPIExam/src/main/java/org/example/JDBC06.java
+++ FCAPIExam/src/main/java/org/example/JDBC06.java
@@ -1,2 +1,27 @@
-package org.example;public class JDBC06 {
+package org.example;
+
+import org.example.model.Book;
+import org.example.model.BookDAO;
+
+// DAO
+public class JDBC06 {
+    public static void main(String[] args) {
+        // 책 데이터를 받아서 DB 에 저장하시오
+
+        Book book = new Book();
+        book.setTitle("인공지능");
+        book.setCompany("영진출판사");
+        book.setName("이길동");
+        book.setPrice(31000);
+
+        // DAO : DB 비지니스 로직(Service)
+        BookDAO dao = new BookDAO();
+        int cnt = dao.bookRegister(book);
+
+        if(cnt > 0){
+            System.out.println("저장 성공");
+        }else{
+            System.out.println("저장 실패");
+        }
+    }
 }
