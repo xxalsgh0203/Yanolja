2d0c65a2b2a8e7846c8008ccb0044fc39d1460bd
Minho Kim <music4537@naver.com>
2023-08-31 14:19:04 +0900

    스프링 데이터 JPA

--- Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -13,24 +13,26 @@ import javax.sql.DataSource;
 public class SpringConfig {
     // DataSource는 데이터베이스 커넥션을 획득할 때 사용하는 객체다.
     // 스프링 부트는 데이터베이스 커넥션 정보를 바탕으로 DataSource를 생성하고 스프링 빈으로 만들어둔다. 그래서 DI를 받을 수 있다
-    private final DataSource dataSource;
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
+//    private final DataSource dataSource;
+//    private final EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource, EntityManager em){
-        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository){
+//        this.dataSource = dataSource;
+//        this.em = em;
+        this.memberRepository = memberRepository;
     }
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
 
 }
