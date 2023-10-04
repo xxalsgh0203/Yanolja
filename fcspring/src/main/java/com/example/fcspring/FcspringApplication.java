15c163ad49a59dead999154f27fdff6f3551ce12
Minho Kim <music4537@naver.com>
2023-10-04 12:33:27 +0900

    스프링 프로젝트 생성

---
+++ fcspring/src/main/java/com/example/fcspring/FcspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.fcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcspringApplication.class, args);
+	}
+
+}
---
+++ fcspring/src/test/java/com/example/fcspring/FcspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
